1f5dde7027b6c32dc97553ebf70d6008c17a036a
Vincent <mijnleraar@msn.com>
2013-07-22 22:06:14 +0200

    teleport() method will now send effects.

--- src/me/staartvin/scrollteleportation/teleporthandler/TeleportHandler.java
+++ src/me/staartvin/scrollteleportation/teleporthandler/TeleportHandler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import me.staartvin.scrollteleportation.ScrollTeleportation;
+import me.staartvin.scrollteleportation.exceptions.PotionEffectInvalidException;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -55,7 +56,12 @@ public class TeleportHandler {
 			decreaseUse(item, player);
 			
 			// Play effects
-			//player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100, 1));
+			try {
+				applyEffects(player, plugin.getMainConfig().getScroll(plugin.fixName(item.getItemMeta().getDisplayName())));
+			} catch (PotionEffectInvalidException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -113,5 +119,59 @@ public class TeleportHandler {
 			// Give new item back (1 less use)
 			player.getInventory().addItem(item);
 		}
+	}
+	
+	public void applyEffects(Player player, String scroll) throws PotionEffectInvalidException {
+		List<String> effects = plugin.getMainConfig().getEffects(scroll);
+		
+		for (String effect: effects) {
+			String[] args = effect.split(" ");
+			
+			if (args.length != 2) {
+				throw new PotionEffectInvalidException("Missing duration argument for '" + effect + "'!");
+			}
+			
+			String realEffect = args[0].trim();
+			int duration = 1;
+			
+			try {
+				duration = Integer.parseInt(args[1].trim());
+			} catch (Exception e) {
+				throw new PotionEffectInvalidException("Invalid duration for '" + effect + "'!");
+			}
+			
+			PotionEffectType potionEffect = matchPotionEffect(realEffect);
+			
+			if (potionEffect == null) {
+				throw new PotionEffectInvalidException("PotionEffect '" + realEffect + "' is not a valid effect!");
+			}
+			
+			// Play effect 
+			player.addPotionEffect(new PotionEffect(potionEffect, duration * 20, 1));
+		}	
+	}
+	
+	public PotionEffectType matchPotionEffect(String effectName) {
+		if (effectName.equalsIgnoreCase("speed")) return PotionEffectType.SPEED;
+		else if (effectName.equalsIgnoreCase("slow")) return PotionEffectType.SLOW;
+		else if (effectName.equalsIgnoreCase("fast_digging")) return PotionEffectType.FAST_DIGGING;
+		else if (effectName.equalsIgnoreCase("slow_digging")) return PotionEffectType.SLOW_DIGGING;
+		else if (effectName.equalsIgnoreCase("increase_damage")) return PotionEffectType.INCREASE_DAMAGE;
+		else if (effectName.equalsIgnoreCase("heal")) return PotionEffectType.HEAL;
+		else if (effectName.equalsIgnoreCase("harm")) return PotionEffectType.HARM;
+		else if (effectName.equalsIgnoreCase("jump")) return PotionEffectType.JUMP;
+		else if (effectName.equalsIgnoreCase("confusion")) return PotionEffectType.CONFUSION;
+		else if (effectName.equalsIgnoreCase("regeneration")) return PotionEffectType.REGENERATION;
+		else if (effectName.equalsIgnoreCase("damage_resistance")) return PotionEffectType.DAMAGE_RESISTANCE;
+		else if (effectName.equalsIgnoreCase("fire_resistance")) return PotionEffectType.FIRE_RESISTANCE;
+		else if (effectName.equalsIgnoreCase("water_breathing")) return PotionEffectType.WATER_BREATHING;
+		else if (effectName.equalsIgnoreCase("invisibility")) return PotionEffectType.INVISIBILITY;
+		else if (effectName.equalsIgnoreCase("blindness")) return PotionEffectType.BLINDNESS;
+		else if (effectName.equalsIgnoreCase("night_vision")) return PotionEffectType.NIGHT_VISION;
+		else if (effectName.equalsIgnoreCase("hunger")) return PotionEffectType.HUNGER;
+		else if (effectName.equalsIgnoreCase("weakness")) return PotionEffectType.WEAKNESS;
+		else if (effectName.equalsIgnoreCase("poison")) return PotionEffectType.POISON;
+		else if (effectName.equalsIgnoreCase("wither")) return PotionEffectType.WITHER;
+		else return null;
 	}
 }
