90ef9725d87693f64170581a16be3a40f8cd0c8b
Staartvin <mijnleraar@msn.com>
2013-12-01 14:37:09 +0100

    Test commit
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/staartvin/scrollteleportation/ScrollTeleportation.java
+++ src/me/staartvin/scrollteleportation/ScrollTeleportation.java
@@ -24,6 +24,8 @@ public class ScrollTeleportation extends JavaPlugin {
 	private TeleportHandler teleHandler = new TeleportHandler(this);
 	private DestinationHandler desHandler = new DestinationHandler(this);
 	
+	// Test push
+	
 	public void onEnable() {
 		// Load configuration file
 		config.loadConfiguration();
