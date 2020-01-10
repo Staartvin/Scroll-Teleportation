cd142ed3334b281af09a5a74a667adc79005af30
Staartvin <mijnleraar@msn.com>
2020-01-10 11:22:07 +0100

    Remove code that was not needed anymore.

--- src/me/staartvin/scrollteleportation/listeners/PlayerInvOpenListener.java
+++ src/me/staartvin/scrollteleportation/listeners/PlayerInvOpenListener.java
@@ -1,7 +1,6 @@
 package me.staartvin.scrollteleportation.listeners;
 
 import me.staartvin.scrollteleportation.ScrollTeleportation;
-
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,45 +10,39 @@ import org.bukkit.event.inventory.InventoryType;
 
 public class PlayerInvOpenListener implements Listener {
 
-	private ScrollTeleportation plugin;
+    private ScrollTeleportation plugin;
 
-	public PlayerInvOpenListener(ScrollTeleportation instance) {
-		plugin = instance;
-	}
+    public PlayerInvOpenListener(ScrollTeleportation instance) {
+        plugin = instance;
+    }
 
-	@EventHandler
-	public void onInventoryOpen(InventoryOpenEvent event) {
-		Player player = (Player) event.getPlayer();
+    @EventHandler
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        Player player = (Player) event.getPlayer();
 
-		if (event.getInventory().getType() == InventoryType.PLAYER)
-			return;
+        if (event.getInventory().getType() == InventoryType.PLAYER)
+            return;
 
-		if (plugin.getTeleportHandler().isReady(player.getName())) {
+        if (plugin.getTeleportHandler().isReady(player.getName())) {
 
-			if (player.hasPermission("scrollteleportation.invbypass"))
-				return;
+            if (player.hasPermission("scrollteleportation.invbypass"))
+                return;
 
-			//if (!plugin.getMainConfig().doCancelOnMove(scroll))
-			//return;
+            // Player has moved so teleportation is cancelled
+            plugin.getTeleportHandler().setReady(player.getName(), false);
 
-			// Player has moved so teleportation is cancelled
-			plugin.getTeleportHandler().setReady(player.getName(), false);
+            if (plugin.getTeleportHandler().taskID.get(player.getName()) != null) {
+                // Cancel teleport task
+                plugin.getServer().getScheduler().cancelTask(plugin.getTeleportHandler().taskID.get(player
+                        .getName()));
 
-			if (plugin.getTeleportHandler().taskID.get(player.getName()) != null) {
-				// Cancel teleport task
-				plugin.getServer()
-						.getScheduler()
-						.cancelTask(
-								plugin.getTeleportHandler().taskID.get(player
-										.getName()));
+                // Set taskID null
+                plugin.getTeleportHandler().taskID.put(player.getName(), null);
+            }
 
-				// Set taskID null
-				plugin.getTeleportHandler().taskID.put(player.getName(), null);
-			}
-
-			// Inform player
-			player.sendMessage(ChatColor.RED
-					+ "Teleportation is cancelled because you opened an inventory.");
-		}
-	}
+            // Inform player
+            player.sendMessage(ChatColor.RED
+                    + "Teleportation is cancelled because you opened an inventory.");
+        }
+    }
 }
